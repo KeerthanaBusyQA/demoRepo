199a92667a38ab913113df9ec170d93e54ff3ec3
unknown <keerthana.busyqa@gmail.com>
2018-09-14 17:00:52 -0400

    First git commit

---
+++ src/test/java/packageJenkins/Hello2.java
@@ -0,0 +1,16 @@
+package packageJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Hello2 {
+@Test
+public void hello() {
+	System.out.println("Let us learn Continuous Integration");
+	/*System.setProperty("webdriver.chrome.driver", "E:\\Softwares\\Drivers\\ChromeDriver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://www.google.com");
+	driver.close();*/
+}
+}
